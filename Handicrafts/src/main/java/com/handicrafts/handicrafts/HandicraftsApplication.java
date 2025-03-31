e19a3c39f512badc2a7b58be7b2cc657086d64e3
NDsPr <144921933+NDsPr@users.noreply.github.com>
2025-03-31 23:02:41 +0700

    Create Project Handicrafts

---
+++ Handicrafts/src/main/java/com/handicrafts/handicrafts/HandicraftsApplication.java
@@ -0,0 +1,13 @@
+package com.handicrafts.handicrafts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandicraftsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandicraftsApplication.class, args);
+    }
+
+}
---
+++ Handicrafts/src/test/java/com/handicrafts/handicrafts/HandicraftsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handicrafts.handicrafts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandicraftsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
