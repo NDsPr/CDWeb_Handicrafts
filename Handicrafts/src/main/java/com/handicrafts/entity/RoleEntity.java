923dd5f630d633dbcc3ebe6753dadf0943ef3d06
NDsPr <21130336@st.hcmuaf.edu.vn>
2025-04-12 23:31:34 +0700

    User
    Các xử lý liên quan đến việc thực thi UserService

---
+++ Handicrafts/src/main/java/com/handicrafts/entity/RoleEntity.java
@@ -0,0 +1,26 @@
+
+package com.handicrafts.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "role")
+@Getter
+@Setter
+@NoArgsConstructor
+public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int roleID;
+    @Column(name = "name")
+    private String name;
+    @ManyToMany(mappedBy = "roles")
+    private List<UserEntity> users = new ArrayList<>();
+
+}
