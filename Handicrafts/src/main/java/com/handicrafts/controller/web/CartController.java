865b689ffd9c7a1dde4cce387b1062509970231c
LeNgocThanhDao09 <21130309@st.hcmuaf.edu.vn>
2025-05-27 18:41:14 +0700

    taoj các controller Cart, Category, Handicraft

--- Handicrafts/src/main/java/com/handicrafts/controller/web/CartController.java
+++ Handicrafts/src/main/java/com/handicrafts/controller/web/CartController.java
@@ -1,4 +1,97 @@
 package com.handicrafts.controller.web;
 
+
+import com.handicrafts.api.output.CartOutput;
+import com.handicrafts.dto.CartDTO;
+import com.handicrafts.dto.HandicraftDTO;
+import com.handicrafts.oauth2.CustomOAuth2User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CartController {
+
+    @Autowired
+    private ICartService cartService;
+
+    @GetMapping("/them-san-pham")
+    public CartDTO addProduct(@RequestParam(name = "handicraftID") int handicraftId,
+                              @RequestParam(name = "quantity") int quantity,
+                              Authentication authentication) {
+        if (authentication != null) {
+            String userEmail = getUserEmail(authentication);
+            return cartService.addHandicraft(userEmail, handicraftId, quantity);
+        }
+        CartDTO result = new CartDTO();
+        result.setHandicraft(new HandicraftDTO());
+        return result;
+    }
+
+    @GetMapping("/get-san-pham")
+    public CartOutput getProducts(Authentication authentication) {
+        if (authentication == null) return null;
+        String userEmail = getUserEmail(authentication);
+
+        List<CartDTO> handicraftsDb = cartService.getHandicrafts(userEmail);
+        double total = 0.0;
+        for (CartDTO c : handicraftsDb) {
+            total += c.getHandicraft().getPrice() * (1 - (c.getHandicraft().getDiscountPercent() / 100)) * c.getQuantity();
+        }
+
+        CartOutput output = new CartOutput();
+        output.setTotal(total);
+        output.setCartList(handicraftsDb);
+        return output;
+    }
+
+    @GetMapping("/xoa-san-pham")
+    public CartOutput deleteProduct(@RequestParam(name = "handicraftID") int handicraftId,
+                                    Authentication authentication) {
+        if (authentication == null) return null;
+        String userEmail = getUserEmail(authentication);
+
+        List<CartDTO> cartDeleted = cartService.deleteHandicraft(userEmail, handicraftId);
+        double total = 0.0;
+        for (CartDTO c : cartDeleted) {
+            total += c.getHandicraft().getPrice() * (1 - (c.getHandicraft().getDiscountPercent() / 100)) * c.getQuantity();
+        }
+
+        CartOutput outputDelete = new CartOutput();
+        outputDelete.setTotal(total);
+        outputDelete.setCartList(cartDeleted);
+        return outputDelete;
+    }
+
+    @GetMapping("/cap-nhat-so-luong")
+    public CartOutput updateQuantity(@RequestParam(name = "handicraftID") int handicraftId,
+                                     @RequestParam(name = "quantity") int newQuantity,
+                                     Authentication authentication) {
+        if (authentication == null) return null;
+        String userEmail = getUserEmail(authentication);
+
+        List<CartDTO> cartUpdate = cartService.updateQuantity(userEmail, handicraftId, newQuantity);
+        double total = 0.0;
+        for (CartDTO c : cartUpdate) {
+            total += c.getHandicraft().getPrice() * (1 - (c.getHandicraft().getDiscountPercent() / 100)) * c.getQuantity();
+        }
+
+        CartOutput outputUpdate = new CartOutput();
+        outputUpdate.setTotal(total);
+        outputUpdate.setCartList(cartUpdate);
+        return outputUpdate;
+    }
+
+    private String getUserEmail(Authentication authentication) {
+        if (authentication.getPrincipal() instanceof CustomOAuth2User) {
+            CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+            return oAuth2User.getAttribute("email");
+        }
+        return authentication.getName();
+    }
 }
+
--- Handicrafts/src/main/java/com/handicrafts/controller/web/HandicraftController.java
+++ Handicrafts/src/main/java/com/handicrafts/controller/web/HandicraftController.java
@@ -1,4 +1,117 @@
 package com.handicrafts.controller.web;
 
+
+import com.handicrafts.api.output.HandicraftOutput;
+import com.handicrafts.dto.HandicraftDTO;
+import com.handicrafts.service.IHandicraftService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@RestController
 public class HandicraftController {
+
+    @Autowired
+    private IHandicraftService handicraftService;
+
+    @GetMapping("/danh-sach-san-pham")
+    public HandicraftOutput findAll(
+            @RequestParam(name = "category", required = false, defaultValue = "null") String category,
+            @RequestParam(name = "category_type", required = false, defaultValue = "null") String categoryType,
+            @RequestParam(name = "material", required = false, defaultValue = "null") String material,
+            @RequestParam(name = "title", required = false, defaultValue = "null") String title,
+            @RequestParam(name = "popular", required = false, defaultValue = "false") boolean isPopular,
+            @RequestParam(name = "new_arrival", required = false, defaultValue = "false") boolean isNewArrival,
+            @RequestParam(name = "sort", required = false, defaultValue = "id") String sortBy,
+            @RequestParam(name = "order", required = false, defaultValue = "asc") String sortOrder,
+            @RequestParam(name = "from", required = false) Integer from,
+            @RequestParam(name = "to", required = false) Integer to,
+            @RequestParam(name = "discount", required = false, defaultValue = "0") Double discount
+    ) {
+        final int SIZE = 9;
+        HandicraftOutput output = new HandicraftOutput();
+
+        Sort sort = sortOrder.equalsIgnoreCase("asc") ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(0, SIZE, sort); // Không phân trang thực, chỉ lấy 1 trang 9 SP
+
+        if (!title.equalsIgnoreCase("null")) {
+            output.setResult(handicraftService.findAllByTitleContaining(title, pageable));
+            output.setTotalPage((int) Math.ceil((double) handicraftService.countAllByTitleContaining(title) / SIZE));
+
+        } else if (!category.equalsIgnoreCase("null")) {
+            output.setResult(handicraftService.findByCategoryCode(category, pageable));
+            output.setTotalPage((int) Math.ceil((double) handicraftService.countByCategory(category) / SIZE));
+
+        } else if (!categoryType.equalsIgnoreCase("null")) {
+            output.setResult(handicraftService.findByCategoryTypeCode(categoryType, pageable));
+            output.setTotalPage((int) Math.ceil((double) handicraftService.countByCategoryType(categoryType) / SIZE));
+
+        } else if (!material.equalsIgnoreCase("null")) {
+            output.setResult(handicraftService.findByMaterial(material, pageable));
+            output.setTotalPage((int) Math.ceil((double) handicraftService.countByMaterial(material) / SIZE));
+
+        } else if (from != null && to != null) {
+            output.setResult(handicraftService.findByPriceBetween(from, to, pageable));
+            output.setTotalPage((int) Math.ceil((double) handicraftService.countByPriceBetween(from, to) / SIZE));
+
+        } else if (from != null) {
+            output.setResult(handicraftService.findByPriceGreaterThan(from, pageable));
+            output.setTotalPage((int) Math.ceil((double) handicraftService.countByPriceGreaterThan(from) / SIZE));
+
+        } else if (isPopular) {
+            output.setResult(handicraftService.findAllPopular(true, pageable));
+            output.setTotalPage((int) Math.ceil((double) handicraftService.countPopular(true) / SIZE));
+
+        } else if (isNewArrival) {
+            output.setResult(handicraftService.findAllNewArrival(true, pageable));
+            output.setTotalPage((int) Math.ceil((double) handicraftService.countNewArrival(true) / SIZE));
+
+        } else if (discount != 0) {
+            output.setResult(handicraftService.findByDiscountGreaterThan(discount, pageable));
+            output.setTotalPage((int) Math.ceil((double) handicraftService.countByDiscountGreaterThan(discount) / SIZE));
+
+        } else {
+            output.setResult(handicraftService.findAll(pageable));
+            output.setTotalPage((int) Math.ceil((double) handicraftService.countAllActive() / SIZE));
+        }
+
+        return output;
+    }
+
+    @GetMapping("/san-pham")
+    public ModelAndView viewShopPage() {
+        return new ModelAndView("web/shop.html");
+    }
+
+    @GetMapping("/san-pham-noi-bat")
+    public List<HandicraftDTO> findPopularItems() {
+        return handicraftService.findPopularItems(true);
+    }
+
+    @GetMapping("/san-pham-moi")
+    public List<HandicraftDTO> findNewArrivals() {
+        return handicraftService.findNewArrivals(true);
+    }
+
+    @GetMapping("/autocomplete")
+    public List<String> autocompleteTitle(@RequestParam("title") String title) {
+        return handicraftService.autocompleteTitle(title);
+    }
+
+    @GetMapping("/chi-tiet")
+    public ModelAndView viewDetail(@RequestParam("id") Integer id) {
+        ModelAndView mav = new ModelAndView("web/detail.html");
+        HandicraftDTO item = handicraftService.findById(id);
+        mav.addObject("list", handicraftService.findSimilarItems(item.getCategory().getCategoryID(), 50));
+        return mav;
+    }
+
+    @GetMapping("/getDetail")
+    public HandicraftDTO getDetail(@RequestParam("id") int id) {
+        return handicraftService.findById(id);
+    }
 }
+
