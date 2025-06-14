772d74c9c3b0b7762f3755c58497638a37b0ea9f
LeNgocThanhDao09 <21130309@st.hcmuaf.edu.vn>
2025-06-15 00:50:01 +0700

    chỉnh sửa và hoàn thiện chức năng tìm kiếm và cảm ơn vì đã thanh toán

---
+++ Handicrafts/src/main/java/com/handicrafts/controller/web/ThankYouOrderController.java
@@ -0,0 +1,26 @@
+package com.handicrafts.controller.web;
+
+import com.handicrafts.dto.CustomizeDTO;
+import com.handicrafts.repository.CustomizeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThankYouOrderController {
+
+    private final CustomizeRepository customizeRepository;
+
+    @Autowired
+    public ThankYouOrderController(CustomizeRepository customizeRepository) {
+        this.customizeRepository = customizeRepository;
+    }
+
+    @GetMapping("/thankyou")
+    public String showThankYouPage(Model model) {
+        CustomizeDTO customizeInfo = customizeRepository.getCustomizeInfo();
+        model.addAttribute("customizeInfo", customizeInfo);
+        return "thankyou"; // Tên file thankyou.jsp hoặc thankyou.html nếu dùng Thymeleaf
+    }
+}
