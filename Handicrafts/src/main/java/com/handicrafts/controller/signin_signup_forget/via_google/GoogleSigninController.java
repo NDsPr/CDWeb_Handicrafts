1409c895f8cb49e749f794d897cc945ba78f5666
NDsPr <21130336@st.hcmuaf.edu.vn>
2025-06-16 00:57:25 +0700

    Cập nhật phương thức ĐK, ĐN bởi Google

--- Handicrafts/src/main/java/com/handicrafts/controller/signin_signup_forget/via_google/GoogleSigninController.java
+++ Handicrafts/src/main/java/com/handicrafts/controller/signin_signup_forget/via_google/GoogleSigninController.java
@@ -1,25 +1,32 @@
 package com.handicrafts.controller.signin_signup_forget.via_google;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ResourceBundle;
 
-// Servlet nhận request login via Google và chuyển hướng về Google
-@WebServlet(value = {"/signin-via-google"})
-public class GoogleSigninController extends HttpServlet {
-    private static ResourceBundle bundle = ResourceBundle.getBundle("oauth2dot0");
+/**
+ * Controller nhận request login via Google và chuyển hướng về Google
+ */
+@Controller
+@RequestMapping("/signin-via-google")
+public class GoogleSigninController {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String clientId = bundle.getString("google-client-id");
-        String redirectUri = bundle.getString("google-redirect-uri");
-        String googleLoginUrl = "https://accounts.google.com/o/oauth2/auth?client_id=" + clientId
-                + "&response_type=code&redirect_uri=" + redirectUri + "&scope=email%20profile&access_type=offline";
+    @Value("${oauth2dot0.google-client-id}")
+    private String googleClientId;
 
-        resp.sendRedirect(googleLoginUrl);
+    @Value("${oauth2dot0.google-redirect-uri}")
+    private String googleRedirectUri;
+
+    @GetMapping
+    public void signinViaGoogle(HttpServletResponse response) throws IOException {
+        String googleLoginUrl = "https://accounts.google.com/o/oauth2/auth?client_id=" + googleClientId
+                + "&response_type=code&redirect_uri=" + googleRedirectUri
+                + "&scope=email%20profile&access_type=offline";
+
+        response.sendRedirect(googleLoginUrl);
     }
 }
