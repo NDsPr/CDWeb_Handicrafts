f1e73f37c0fe84758ca66b060d61debaba789799
NDsPr <21130336@st.hcmuaf.edu.vn>
2025-06-16 23:56:25 +0700

    Update các phần liên quan đến Log trong service

---
+++ Handicrafts/src/main/java/com/handicrafts/service/ILogService.java
@@ -0,0 +1,21 @@
+package com.handicrafts.service;
+
+import com.handicrafts.dto.LogDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface ILogService<T> {
+    void log(HttpServletRequest request, String function, String state, int level, T previous, T current);
+
+    int save(LogDTO dto);
+
+    List<LogDTO> findAll();
+
+    LogDTO findById(Integer id);
+
+    void deleteById(Integer id);
+
+    Page<LogDTO> searchLogs(String keyword, int page, int size, String sortField, String sortDir);
+}
